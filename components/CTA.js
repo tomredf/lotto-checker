e060ff1366617a05911df6014c601e67ecb791e5
dermotfitzpatrick <tomredf@gmail.com>
2022-04-06 01:08:25 -0700

    Second commit

--- components/CTA.js
+++ components/CTA.js
@@ -1,7 +1,7 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
 
-export default function Example() {
+export default function CTA() {
   return (
     <div className="">
       <div className="mx-auto max-w-7xl py-12 px-0 sm:px-4 lg:flex lg:items-center lg:justify-between lg:py-12 lg:px-0">
@@ -11,10 +11,12 @@ export default function Example() {
         </h2>
         <div className="mt-8 lg:mt-0">
           <div className="inline-flex rounded-md shadow">
-            <Link href="/" aria-label="About">
-              className="inline-flex items-center justify-center rounded-md border
-              border-transparent bg-primary-500 px-5 py-3 text-base font-medium text-white
-              hover:bg-primary-600" > Get started now
+            <Link
+              href="/contactus"
+              aria-label="Contact"
+              className="inline-flex items-center justify-center rounded-md border border-transparent bg-primary-500 px-5 py-3 text-base font-medium text-white hover:bg-primary-600"
+            >
+              Get started now
             </Link>
           </div>
         </div>
