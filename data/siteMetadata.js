3033be1600e188b987ed9afc46fe01dfa1db1194
dermotfitzpatrick <tomredf@gmail.com>
2022-04-06 23:39:23 -0700

    Added favicon

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -5,7 +5,7 @@ const siteMetadata = {
   description: 'Solutions for small business',
   language: 'en-ca',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
+  siteUrl: 'https://clarasoftware.com',
   //siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
