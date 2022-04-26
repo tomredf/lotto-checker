c6e7a5dbfe5df975db6a3610de99a0fd728c3f82
dermotfitzpatrick <tomredf@gmail.com>
2022-04-26 10:27:19 -0700

    Adding usebasin for contact form

---
+++ pages/thankyou.js
@@ -0,0 +1,16 @@
+import Contact from '@/components/Contact'
+
+export default function ThankYou() {
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="mb-8 space-y-2 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-primary-500 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+          Thank You
+        </h1>
+        <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+          Thank you for contacting us. We will be in touch shortly.
+        </p>
+      </div>
+    </div>
+  )
+}
