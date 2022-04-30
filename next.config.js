a07b5b7d62380c3e27db4209cda60629457a18d3
dermotfitzpatrick <tomredf@gmail.com>
2022-04-29 23:21:53 -0700

    Adding Plausible

--- next.config.js
+++ next.config.js
@@ -5,15 +5,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' discus.app plausible.io;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' discus.app plausible.io/js/plausible.js;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
   connect-src *;
-  font-src 'self';
   frame-src cal.com
 `
-
+//font-src 'self';
 const securityHeaders = [
   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
   {
